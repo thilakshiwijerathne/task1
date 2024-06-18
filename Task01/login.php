81141b2656b44e9d426bdd75d53328de4a78a864
thilakshiwijerathne <157473460+thilakshiwijerathne@users.noreply.github.com>
2024-06-18 10:07:16 +0530

    Update login.php

--- Task01/login.php
+++ Task01/login.php
@@ -1,26 +1,4 @@
 <?php
-    $con = mysqli_connect("localhost","root","");
-    if (!$con)
-    {
-        die('Could not connect: ' . mysqli_error($con));
-    }
-    mysqli_select_db($con , "blissdatabase");
-    $result = mysqli_query($con, "SELECT pword FROM registation
-    WHERE email ='$_POST[email]'");
-    while($row = mysqli_fetch_array($result))
-    {
-    $password= $row['pword'];
-    }
-    
-    $pa= htmlspecialchars($_POST['password']);
-
-    if($pa==$password){
-        header("Location: cart.html");
-    }
-    else{  
-        echo '<script>alert("Wrong password and username!");</script>';
-    }
-
-    mysqli_close($con);
+   
     
 ?>
